dfe34c2aa37220006624a98aff63510513bf68ea
Riccardo Bertolini <info@riccardobertolini.net>
2022-11-27 14:13:11 +0100

    feat: fixing typo

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export const getIBMdata = async () => {
 
 export const manipulateData = (dataArray: any) => (
   dataArray.data.map((element: any, index:number) => ({
-    name: `${index >= 20 ? index/20 + 'h our(s)': index * 5 + 'mins'}`,
+    name: `${index >= 20 ? index/20 + 'hour(s)': index * 5 + 'mins'}`,
     //ts-ignore
     open: parseFloat(Object.values(element)[0] as any),
     close: parseFloat(Object.values(element)[3] as any)
@@ -74,8 +74,8 @@ export const App = () => {
               <YAxis domain={[getYmin(APIData), getYmax(APIData)]} tick={false}  />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="open" stroke="red" fill="#8884d8"/> 
-              <Line type="monotone" dataKey="close" stroke="grey " fill="#8884d8"  /> 
+              <Line type="monotone" dataKey="open" stroke="red"  strokeWidth={1} dot={false}/> 
+              <Line type="monotone" dataKey="close" stroke="grey " strokeWidth={1} dot={false}/> 
             </LineChart> }
     </div>
   </>
